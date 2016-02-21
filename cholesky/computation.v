f15622e80bbe59f7bb4959e56f256f6b38546cbf
Pierre Roux <pierre@roux01.fr>
2016-02-21 12:16:23 +0100

    Implementation of cholesky_prog with seq.

--- cholesky/computation.v
+++ cholesky/computation.v
@@ -77,6 +77,98 @@ Definition F64 := binary64_infnan.
 
 Require Import matrix seqmatrix refinements.
 
+Section seq_cholesky.
+
+Fixpoint seq_stilde k c a b :=
+  match k, a, b with
+    | O, _, _ => c
+    | S k, [::], _ => c
+    | S k, _, [::] => c
+    | S k, a1 :: a2, b1 :: b2 => seq_stilde k (fiplus c (fiopp (fimult a1 b1))) a2 b2
+  end.
+
+Definition seq_ytilded k c a b bk := fidiv (seq_stilde k c a b) bk.
+
+Definition seq_ytildes k c a := fisqrt (seq_stilde k c a a).
+
+Fixpoint seq_store T s n (v : T) :=
+  match n, s with
+    | _, [::] => [::]
+    | O, _ :: t => v :: t
+    | S n, h :: t => h :: seq_store t n v
+  end.
+
+Fixpoint store T m i j (v : T) :=
+  match i, m with
+    | _, [::] => [::]
+    | O, h :: t => seq_store h j v :: t
+    | S i, h :: t => h :: store t i j v
+  end.
+
+Definition m_id := [:: [:: one; FI0]; [:: FI0; one]].
+Definition m_0 := [:: [:: FI0; FI0]; [:: FI0; FI0]].
+
+Time Eval vm_compute in map (map B2F) (store m_id 0 1 half).
+
+Require Import Recdef.
+
+(* note: R is transposed with respect to cholesky.v *)
+Section InnerLoop.
+Variable j : nat.
+Function inner_loop A R (i : nat)
+         {measure (fun i => (j - i)%N) i} :=
+  if (i < j)%N then
+    let R := store R j i (seq_ytilded i (nth FI0 (nth [::] A i) j)
+                                      (nth [::] R i) (nth [::] R j)
+                                      (nth FI0 (nth [::] R i) i)) in
+    inner_loop A R (i + 1)
+  else
+    R.
+move=> _ _ i H; apply /ltP; rewrite addn1 subnSK //.
+Defined.
+End InnerLoop.
+
+Section OuterLoop.
+Variable n : nat.
+Function outer_loop A R (j : nat)
+         {measure (fun i => (n - j)%N) j} :=
+  if (j < n)%N then
+    let R := inner_loop j A R 0 in
+    let R := store R j j (seq_ytildes j (nth FI0 (nth [::] A j) j)
+                                      (nth [::] R j)) in
+    outer_loop A R (j + 1)
+  else
+    R.
+move=> _ _ j H; apply /ltP; rewrite addn1 subnSK //.
+Defined.
+End OuterLoop.
+
+(* note: the result is transposed with respect to cholesky.v *)
+Definition cholesky A :=
+  let sz := size A in
+  outer_loop sz A A 0.
+
+Time Eval vm_compute in map (map B2F) (cholesky m_id).
+
+Definition m2 := [:: [:: Z2B 2; Z2B (-3); Z2B 1]; [:: Z2B (-3); Z2B 5; Z2B 0]; [:: Z2B 1; Z2B 0; Z2B 5]].
+
+Time Eval vm_compute in map (map B2F) (cholesky m2).
+
+(* returns approx:
+
+[ sqrt 2,  -3,     1;
+  -2.1213, 0.7071, 0;
+  0.7071,  2.1213, 0 ]
+
+then R is almost:
+
+1 / sqrt 2 * [ 2, -3, 1;
+               0,  1, 3;
+               0,  0, 0 ]
+*)
+
+End seq_cholesky.
+
 Section GenericFcmdotprod.
 
 Local Open Scope ring_scope.
