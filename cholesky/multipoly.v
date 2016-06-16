b0508dcaeb892dd95db3b8236ae739b4a7442687
Erik Martin-Dorel <erik.martin-dorel@ens-lyon.org>
2016-06-16 18:19:30 +0200

    Define more operations on multinomials, Import FMaps stdlib & Define effmpoly.

--- cholesky/multipoly.v
+++ cholesky/multipoly.v
@@ -1,10 +1,27 @@
+Require Import FMaps FMapAVL.
 From mathcomp Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq.
-From mathcomp Require Import choice finfun tuple fintype.
-From SsrMultinomials Require Import mpoly.
+From mathcomp Require Import choice finfun tuple fintype ssralg.
+From SsrMultinomials Require Import mpoly freeg.
 From CoqEAL_theory Require Import hrel.
 From CoqEAL_refinements Require Import refinements.
 From CoqEAL_refinements Require Import seqmatrix (* for Rord *).
 Require Import misc.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+(** Tip to leverage a Boolean condition *)
+Definition optb (b : bool) : option (is_true b) :=
+  if b then Some erefl else None.
+Definition sumb (b : bool) : {b = true} + {b = false} :=
+  if b is true
+  then left erefl else right erefl.
+Definition sumb' (b : bool) : {b = true} + {b -> False}.
+  refine (if b is true
+          then left erefl else right _).
+  done.
+Defined.
 
 (** Part I: Generic operations *)
 
@@ -23,36 +40,94 @@ Definition mnmd {n} (c : 'I_n) (d : nat) :=
 Definition mnmd_seq {n} (i d : nat) :=
   nseq i 0%N ++ [:: d] ++ nseq (n - i - 1) 0%N.
 
-(* Multiplication of multinomials *)
+(** Multiplication of multinomials *)
 Definition mnm_add_seq m1 m2 := map2 addn m1 m2.
 
-(* Translation of [poset.lt] *)
-Fixpoint mnmc_le_seq (s1 s2 : seq nat) {struct s1} : bool :=
+(** Translation of [poset.ltx] *)
+Fixpoint mnmc_lt_seq (s1 s2 : seq nat) {struct s1} : bool :=
   match s1 with
   | [::] => true
   | x1 :: s1' =>
       match s2 with
       | [::] => false
-      | x2 :: s2' => (x1 <= x2)%N && mnmc_le_seq s1' s2'
+      | x2 :: s2' => (x1 < x2)%N && mnmc_lt_seq s1' s2'
       end
+  end.
+
+(* Maybe redundant with CoqEAL def *)
+Fixpoint mnmc_eq_seq (s1 s2 : seq nat) {struct s1} : bool :=
+  match s1, s2 with
+  | [::], [::] => true
+  | x1 :: s1', x2 :: s2' => (x1 == x2)%N && mnmc_eq_seq s1' s2'
+  | _, _ => false
   end.
 
 End seqmpoly_generic.
 
+(** Multivariate polynomials *)
+
+Module MultinomOrd <: OrderedType.
+Definition t := seqmultinom.
+Definition eq : t -> t -> Prop := mnmc_eq_seq.
+Definition lt : t -> t -> Prop := mnmc_lt_seq.
+
+Lemma intro_eq x y :
+  (mnmc_lt_seq x y = false) -> (mnmc_lt_seq y x = false) -> mnmc_eq_seq x y.
+Proof.
+Admitted.
+
+Definition compare (x y : t) : Compare lt eq x y :=
+  match sumb (mnmc_lt_seq x y) with
+  | left prf => LT prf
+  | right prf =>
+    match sumb (mnmc_lt_seq y x) with
+    | left prf' => GT prf'
+    | right prf' => EQ (intro_eq prf prf')
+    end
+  end.
+
+Definition eq_dec (x y : t) : {eq x y} + {~ eq x y} :=
+  match sumb' (mnmc_eq_seq x y) with
+  | left prf => left prf
+  | right prf => right prf
+  end.
+
+Lemma eq_refl : forall x : t, eq x x.
+Proof.
+elim=> [//|x s IHs].
+by rewrite /eq /mnmc_eq_seq eqxx /=.
+Qed.
+
+Lemma eq_sym : forall x y : t, eq x y -> eq y x.
+Admitted.
+
+Lemma eq_trans : forall x y z : t, eq x y -> eq y z -> eq x z.
+Admitted.
+
+Lemma lt_trans : forall x y z : t, lt x y -> lt y z -> lt x z.
+Admitted.
+
+Lemma lt_not_eq : forall x y : t, lt x y -> ~ eq x y.
+Admitted.
+End MultinomOrd.
+
+Module M := FMapList.Make MultinomOrd.
+(*
+Module M := FMapAVL.Make MultinomOrd.
+*)
+Definition effmpoly := M.t.
+
+
 (** Part II: Proofs for proof-oriented types and programs *)
 Section seqmpoly_theory.
-
-(** Tip to leverage a Boolean condition *)
-Definition optb (b : bool) : option (is_true b) :=
-  if b then Some erefl else None.
-Definition sumb (b : bool) : {b = true} + {b = false} :=
-  if b is true
-  then left erefl else right erefl.
 
 Definition multinom_of_seqmultinom n (m : seqmultinom) : option 'X_{1..n} :=
   if sumb (size m == n) is left prf then
     Some (Multinom (@Tuple n nat m prf))
   else None.
+
+Definition multinom_of_seqmultinom_val n (m : seqmultinom) : 'X_{1..n} :=
+  odflt (@mnm0 n) (multinom_of_seqmultinom n m).
 
 Definition seqmultinom_of_multinom n (m : 'X_{1..n}) :=
   let: Multinom m' := m in tval m'.
@@ -163,8 +238,26 @@ Lemma param_mnm_add n :
 Proof.
 Admitted.
 
-Lemma param_mnmc_le n :
+Lemma param_mnmc_lt n :
   param (Rseqmultinom ==> Rseqmultinom ==> Logic.eq)
-  (@mnmc_le n) mnmc_le_seq.
+  (@mnmc_lt n) mnmc_lt_seq.
 Proof.
 Admitted.
+
+(** Multivariate polynomials *)
+
+Definition mpoly_of_effmpoly (T : ringType) n (p' : effmpoly T) : option (mpoly n T) :=
+  if M.fold (fun k e b => b && (size k == n)%N) p' true then
+    Some [mpoly [freeg [seq (a.2, multinom_of_seqmultinom_val n a.1) | a <- M.elements p']]]
+  else None.
+
+Definition Mt_of_seq T (s : seq (_ * T)) :=
+  foldl (fun acc x => M.add x.1 x.2 acc) (M.empty T) s.
+
+Definition effmpoly_of_mpoly (T : ringType) n (p : mpoly n T) : effmpoly T :=
+  Mt_of_seq [seq (seqmultinom_of_multinom a.2, a.1) | a <- fgenum (val p)].
+
+Definition Reffmpoly `{T : ringType, n : nat} :=
+  ofun_hrel (@mpoly_of_effmpoly T n).
+
+End seqmpoly_theory.
