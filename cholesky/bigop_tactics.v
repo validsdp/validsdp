de53d7302677afd8d1d567fa6034674a8e960648
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2016-07-26 15:52:23 +0200

    Tune the tactic notations to shorten the proof scripts using underbig.
    Usage:
    - underbig (bigop _ _ _) i Hi rewrite ...
    - underbig (bigop _ _ _) ? _ rewrite ...
    
    - underbig in H (bigop _ _ _) i Hi rewrite ...
    - underbig in H (bigop _ _ _) ? _ rewrite ...

--- cholesky/bigop_tactics.v
+++ cholesky/bigop_tactics.v
@@ -10,7 +10,7 @@ From mathcomp Require Import prime binomial ssralg.
 
 (** [under_big] allows one to apply a given tactic under the bigop
     that correspond to the specified arguments. *)
-Ltac under_big b tac :=
+Ltac under_big b x Hx tac :=
   let b' := eval hnf in b in
   match b' with
   | @BigOp.bigop ?R ?I ?idx ?r ?f =>
@@ -21,7 +21,7 @@ Ltac under_big b tac :=
       match goal with
       | [|- ?G b] =>
         refine (@eq_rect_r _ _ G _ b (@eq_bigr R idx op I r P F1 _ _));
-        [|tac;
+        [|move=> x Hx; tac;
           try reflexivity (* instead of "; first reflexivity" *) ];
         cbv beta
       end
@@ -48,16 +48,16 @@ end.
     only the occurrences of [pat] will be rewritten;
     otherwise the occurrences of the first bigop that matches [pat]
     will be rewritten. *)
-Tactic Notation "underbig" open_constr(pat) tactic(tac) :=
+Tactic Notation "underbig" open_constr(pat) simple_intropattern(x) simple_intropattern(Hx) tactic(tac) :=
   tryif match goal with [|- context [pat]] => is_var pat end
-  then under_big pat tac
-  else find_pat pat ltac:(fun b => under_big b tac).
+  then under_big pat x Hx tac
+  else find_pat pat ltac:(fun b => under_big b x Hx tac).
 
 (** ** When the bigop appears in some hypothesis *)
 
 (** [under_big_in] allows one to apply a given tactic under the bigop
     that correspond to the specified arguments, in some hypothesis *)
-Ltac under_big_in b H tac :=
+Ltac under_big_in H b x Hx tac :=
   let b' := eval hnf in b in
   match b' with
   | @BigOp.bigop ?R ?I ?idx ?r ?f =>
@@ -74,14 +74,14 @@ Ltac under_big_in b H tac :=
         suff new : e;
         [ try clear H ; try rename new into H
         | refine (@eq_rect _ _ G H _ (@eq_bigr R idx op I r P F1 _ _));
-          tac;
+          move=> x Hx; tac;
           try reflexivity (* instead of "; first reflexivity" *)
         ]; try unfold e in * |- *; try clear e ; cbv beta
       end
     end
   end.
 
-Ltac find_pat_in pat H tac :=
+Ltac find_pat_in H pat tac :=
   match type of H with context [?x] =>
     unify pat x with typeclass_instances;
     tryif tac x then idtac else fail 2
@@ -92,10 +92,10 @@ end.
     only the occurrences of [pat] will be rewritten;
     otherwise the occurrences of the first bigop that matches [pat]
     will be rewritten. *)
-Tactic Notation "underbig" open_constr(pat) "in" hyp(H) tactic(tac) :=
+Tactic Notation "underbig" "in" hyp(H) open_constr(pat) simple_intropattern(x) simple_intropattern(Hx) tactic(tac) :=
   tryif match type of H with context [pat] => is_var pat end
-  then under_big_in pat H tac
-  else find_pat_in pat H ltac:(fun b => under_big_in b H tac).
+  then under_big_in H pat x Hx tac
+  else find_pat_in H pat ltac:(fun b => under_big_in H b x Hx tac).
 
 (** ** Tests *)
 
@@ -107,12 +107,12 @@ Lemma test1 (n : nat) (R : ringType) (f1 f2 g : nat -> R) :
   \big[+%R/0%R]_(i < n) (f1 i * g i) + \big[+%R/0%R]_(i < n) (f2 i * g i))%R.
 Proof.
 set b1 := {2}(bigop _ _ _).
-Fail underbig b1 move=> *; rewrite GRing.mulrDr.
+Fail underbig b1 ? _ rewrite GRing.mulrDr.
 
-underbig b1 move=> *; rewrite GRing.mulrDl. (* only b1 is rewritten *)
+underbig b1 ? _ rewrite GRing.mulrDl. (* only b1 is rewritten *)
 
 Undo 1. rewrite /b1.
-underbig b1 move=> *; rewrite GRing.mulrDl. (* 3 occurrences are rewritten *)
+underbig b1 ? _ rewrite GRing.mulrDl. (* 3 occurrences are rewritten *)
 
 rewrite big_split /=.
 by rewrite GRing.addrA.
@@ -124,7 +124,7 @@ Lemma test2 (n : nat) (R : fieldType) (f : nat -> R) :
   (\big[+%R/0%R]_(i < n) (f i / f i) = n%:R)%R.
 Proof.
 move=> Hneq0.
-underbig (bigop _ _ _) move=> *; rewrite GRing.divff.
+underbig (bigop _ _ _) ? _ rewrite GRing.divff.
 2: done.
 
 rewrite big_const cardT /= size_enum_ord /GRing.natmul.
@@ -140,12 +140,12 @@ Lemma test3 (n : nat) (R : fieldType) (f : nat -> R) :
 Proof.
 move=> Hneq0 H.
 set b1 := {2}(bigop _ _ _) in H.
-underbig b1 in H move=> i Hi; rewrite GRing.divff. (* only b1 is rewritten *)
+underbig in H b1 ? _ rewrite GRing.divff. (* only b1 is rewritten *)
 done.
 
 Undo 2.
 move: H.
-underbig (bigop _ _ _) move=> *; rewrite GRing.divff.
+underbig b1 ? _ rewrite GRing.divff.
 done.
 
 move=> *; exact: Hneq0.
