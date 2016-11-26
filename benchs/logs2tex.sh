5f66ae99678e8e5446e9e4505b3c1f0aeb833d9a
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2016-11-26 13:59:55 +0100

    Improve the code of logs2tex.sh by following shellcheck's recommendations.

--- benchs/logs2tex.sh
+++ benchs/logs2tex.sh
@@ -6,7 +6,7 @@ TIMEOUT="900"
 
 DESCRS=""
 for d in ${DIRS} ; do
-  DESCRS2=$(cat ${d}/descr | sed -e "/^#/d;s|^|${d}/|;s| |,|g")
+  DESCRS2=$(sed -e "/^#/d;s|^|${d}/|;s| |,|g" < "${d}/descr")
   DESCRS="${DESCRS} ${DESCRS2}"  
 done
 
@@ -15,24 +15,24 @@ printf "Problem & \$n\$ & \$d\$ & \\\\begin{tikzpicture}\\\\node[rotate=90,align
 
 PREVDIR="."
 for fnd in ${DESCRS} ; do
-  f=$(echo ${fnd} | cut -f1 -d,)
-  n=$(echo ${fnd} | cut -f2 -d,)
-  d=$(echo ${fnd} | cut -f3 -d,)
-  DIR=$(echo ${f} | cut -f1 -d/)
-  FILENAME=$(echo ${f} | cut -f2 -d/)
-  if [ ${DIR} != ${PREVDIR} ] ; then
+  f=$(echo "${fnd}" | cut -f1 -d,)
+  n=$(echo "${fnd}" | cut -f2 -d,)
+  d=$(echo "${fnd}" | cut -f3 -d,)
+  DIR=$(echo "${f}" | cut -f1 -d/)
+  FILENAME=$(echo "${f}" | cut -f2 -d/)
+  if [ "${DIR}" != "${PREVDIR}" ] ; then
     printf "\\\\hline\n"
   fi
-  PRF=$(echo ${FILENAME} | sed -e 's/\_/\\_/g')
-  printf "${PRF} & ${n} & ${d}"
+  PRF=$(echo "${FILENAME}" | sed -e 's/\_/\\_/g')
+  printf "%s & %s & %s" "${PRF}" "${n}" "${d}"
   for p in ${PROVERS} ; do
     LOG="${DIR}/${FILENAME}_${p}_${TIMEOUT}s.log"
     OK=0
     TIME="---"
-    if [ -f ${LOG} ] ; then
+    if [ -f "${LOG}" ] ; then
       case $p in
       Sdpa)
-        if $(grep -q "proved: true" ${LOG}) ; then
+        if grep -q "proved: true" "${LOG}" ; then
           OK=1
         fi
         ;;
@@ -42,19 +42,19 @@ for fnd in ${DESCRS} ; do
         fi
         ;;
       Bernstein)
-        NBP=$(grep "Grand Totals: " ${LOG} | sed -e 's/Grand Totals: //;s/ proofs.*//')
-        NBS=$(grep "Grand Totals: " ${LOG} | sed -e 's/.*attempted, //;s/ succeeded.*//')
+        NBP=$(grep "Grand Totals: " "${LOG}" | sed -e 's/Grand Totals: //;s/ proofs.*//')
+        NBS=$(grep "Grand Totals: " "${LOG}" | sed -e 's/.*attempted, //;s/ succeeded.*//')
         if [ "x${NBP}" != "x" -a "x${NBP}" = "x${NBS}" ] ; then
           OK=1
         fi
         ;;
       MC11)
-        if $(grep -q "proved: true" ${LOG}) ; then
+        if grep -q "proved: true" "${LOG}" ; then
           OK=1
         fi
         ;;
       nlc*coq)
-        if $(grep -q "^Inequality ${FILENAME} verified$" ${LOG}) ; then
+        if grep -q "^Inequality ${FILENAME} verified$" "${LOG}" ; then
           OK=1
         fi
         ;;
@@ -65,10 +65,10 @@ for fnd in ${DESCRS} ; do
 	;;
       esac
       if [ ${OK} = "1" ] ; then
-        TIME=$(grep "^\[runlim\] real:" ${LOG} | sed -e 's/^\[runlim\] real:[ \t]*//;s/ seconds//')
+        TIME=$(grep "^\[runlim\] real:" "${LOG}" | sed -e 's/^\[runlim\] real:[ \t]*//;s/ seconds//')
       fi
     fi
-    printf " & ${TIME}"
+    printf " & %s" "${TIME}"
   done
   printf " \\\\\\\\\\n"
   PREVDIR=${DIR}
