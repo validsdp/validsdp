753974d4031df2cfc3ebeece9f86c9559d285531
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2017-07-10 10:51:09 +0200

    Change inductive validsdp_tac_parameters.

--- plugins/soswitness/theories/soswitness.v
+++ plugins/soswitness/theories/soswitness.v
@@ -1,6 +1,9 @@
 Require Export BigQ mathcomp.ssreflect.seq Interval.Interval_specific_ops.
 
 Inductive validsdp_tac_parameters :=
-  s_sdpa | s_csdp | s_mosek.
+| s_sdpa
+| s_csdp
+| s_mosek
+| s_verbose (verbosity: nat).
 
 Declare ML Module "soswitness".
