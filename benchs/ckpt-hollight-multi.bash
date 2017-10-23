aff88331f03feba889d9f7295c89bd7e9238d845
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2017-10-23 14:33:11 +0200

    Add ckpt-hollight-multi.bash (tested with DMTCP 2.5.1)

---
+++ benchs/ckpt-hollight-multi.bash
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+set -meu
+# Erik Martin-Dorel, 2017
+
+function die_hard() {
+    echo -e "$@" >&2
+    exit 1
+}
+
+function stderr() {
+    echo -e "$@" >&2
+}
+
+function usage() {
+    cat >&2 <<EOF
+Usage:
+$ $(basename "$0") HOLLIGHT_DIR
+EOF
+}
+
+function pause() {
+    read -r -p 'Press Enter to continue...'
+}
+
+function ckpt-exit() {
+    fi="$1"
+    stderr "\n$ dmtcp_command --checkpoint"
+    dmtcp_command --checkpoint || true
+    sleep 1s
+    exec 3>&-
+    rm -f "$fi"
+    stderr "The End."
+}
+
+# Possible enhancement: replace output file with a fifo to earn space
+function main() {
+    mlargs=(/usr/bin/ocaml -I +../camlp5)
+    mlcode='#use "../hol.ml";;
+#use "../Multivariate/realanalysis.ml";;
+print_endline "Done.";;'
+    subdir="dmtcp-multi"
+
+    if [[ $# -lt 1 ]]; then
+	usage
+	exit 0
+    fi
+    export HOLLIGHT_DIR="${1%/}"
+    if [[ ! -d "$HOLLIGHT_DIR" ]]; then
+	die_hard "Error: directory '$HOLLIGHT_DIR' doesn't exist."
+    fi
+    if [[ -d "$HOLLIGHT_DIR/$subdir" ]]; then
+	die_hard "Error: directory '$HOLLIGHT_DIR/$subdir' already exists."
+    fi
+
+    cd "$HOLLIGHT_DIR"
+    mkdir "$subdir" && cd "$subdir"
+    fi="$PWD/input"
+    out="$PWD/output"
+
+    if [[ ! -p "$fi" ]]; then
+	mkfifo "$fi"
+    else
+	stderr "Info: fifo '$fi' already exists."
+    fi
+    if [[ -r "$out" ]]; then
+	stderr "Info: file '$out' already exists, saving..."
+	cp --backup=t -fv "$out" "$out"
+    fi
+    >"$out"
+
+    trap 'ckpt-exit "$fi"' EXIT
+
+    stderr "
+The script is about to send the code below to an OCaml REPL:
+================================================================
+$mlcode
+================================================================
+Additional input can be send:
+  echo 'print_endline \"OK\";;' >'$fi'
+The output is shown below by:
+  tail -f '$out'
+In the very end, type ^C to automatically checkpoint & exit.
+"
+    pause
+
+    cat "$fi" | dmtcp_launch "${mlargs[@]}" >"$out" &
+    exec 3>"$fi"
+    echo "$mlcode" > "$fi"
+    tail -f "$out"
+}
+
+main "$@"
