e72c798c8b85b087aeb60e02d4ee391577c8cfbd
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2018-09-12 16:27:45 +0200

    Start benchmarking individual ops times for cholesky with prim floats

--- plugins/soswitness/test-suite/soswitnesstac.v
+++ plugins/soswitness/test-suite/soswitnesstac.v
@@ -17,6 +17,7 @@ Fail soswitness of ([:: ([:: 0; 0; 0; 0], 3%bigQ)], ([::] : seq (seq (seq N * Bi
 
 (*Fail soswitness of ([:: ([:: 0; 0], 3%bigQ); ([:: 0; 2], (10 # 12)%bigQ); ([:: 2; 1], (-12)%bigQ)], ([::] : seq (seq (seq N * BigQ.t_)))) as y'.*)
 
+(*
 soswitness of ([:: ([:: 2; 0], 3%bigQ); ([:: 0; 2], (10 # 12)%bigQ)], ([::] : seq (seq (seq N * BigQ.t_)))) as y.
 soswitness of ([:: ([:: 2; 0], (-1)%bigQ); ([:: 0; 2], (-1)%bigQ); ([:: 0; 0], 4%bigQ)], ([:: [:: ([:: 2; 0], (-1)%bigQ); ([:: 0; 2], (-1)%bigQ); ([:: 0; 0], 2%bigQ)]])) as y'.
 soswitness of ([:: ([:: 1; 0], 1%bigQ); ([:: 0; 1], 1%bigQ); ([:: 0; 0], 1%bigQ)], ([:: [:: ([:: 1; 0], 1%bigQ)]; [:: ([:: 0; 1], 1%bigQ)]])) as y''.
@@ -26,4 +27,5 @@ soswitness of ([:: ([:: 2; 0], (-1)%bigQ); ([:: 0; 2], (-1)%bigQ); ([:: 0; 0], 4
 soswitness of ([:: ([:: 1; 0], 1%bigQ); ([:: 0; 1], 1%bigQ); ([:: 0; 0], 1%bigQ)], ([:: [:: ([:: 1; 0], 1%bigQ)]; [:: ([:: 0; 1], 1%bigQ)]])) as Y'' with (s_sdpa :: nil).
 soswitness_intro of ([:: ([:: 2], 1%bigQ); ([:: 1], (-1)%bigQ)], ([::] : seq (seq (seq N * BigQ.t_)))) as y'''.
 soswitness_intro of ([:: ([:: 2], 1%bigQ); ([:: 1], (-1)%bigQ)], ([:: [:: ([:: 1], 1%bigQ); ([:: 0], (-2)%bigQ)]])) as y''''.
+ *)
 Abort.
--- theories/profile.v
+++ theories/profile.v
@@ -13,34 +13,80 @@ From Bignums Require Import BigZ.
 Local Open Scope bigZ_scope.
 
 Require Import mathcomp.algebra.matrix.
-Require Import CoqEAL_refinements.seqmatrix.
-Require Import CoqEAL_refinements.refinements.
-
-Require Import CoqEAL_theory.hrel.
+Require Import CoqEAL.refinements.seqmx.
+Require Import CoqEAL.refinements.refinements.
+Require Import CoqEAL.refinements.hrel.
+Require Import CoqEAL.refinements.seqmx_complements.
 
 Import Refinements.Op.
 
-Require Import computation testsuite.
+Require Import cholesky_prog iteri_ord posdef_check.
+
+Load matrices.
 
 (* Profile each floating-point arithmetic operation. *)
 
-Section test_CoqInterval_add.
+Lemma blackhole A (t : A) : unit.
+exact tt.
+Qed.
+Notation "§" := (@blackhole _ _).
 
+Section test_CoqInterval_add.
 Local Notation T := F.type.
 
-Instance : add T := fun a b =>
+Definition two := Eval compute in
+      let one := Float 1%bigZ 0%bigZ in
+      F.add rnd_NE 53%bigZ one one.
+Instance : add_of T := (* fun a b =>
   let r1 := F.add rnd_NE 53%bigZ a b in
-  let r2 := F.add rnd_NE 53%bigZ a b in
-  r2.
-Instance : mul T := F.mul rnd_NE 53%bigZ.
-Instance : sqrt T := F.sqrt rnd_NE 53%bigZ.
-Instance : div T := F.div rnd_NE 53%bigZ.
-Instance : opp T := F.neg.
-Instance : zero T := F.zero.
-Instance : Refinements.Op.one T := Float 1%bigZ 0%bigZ.
+  F.div rnd_NE 53%bigZ (F.add rnd_NE 53%bigZ r1 r1) two *)
+F.add rnd_NE 53%bigZ.
+(* Instance : add_of T :=F.add rnd_NE 53%bigZ. *)
+Instance : mul_of T := F.mul rnd_NE 53%bigZ.
+Fixpoint fib (n : nat) :=
+  match n with
+    O => 1%nat
+  | S O => 1%nat
+  | S ((S p) as q) => (fib p + fib q)%nat
+  end.
+Instance : sqrt_of T :=
+  let a := F.sqrt rnd_NE 53%bigZ in
+  a.
+Instance : div_of T := F.div rnd_NE 53%bigZ.
+Instance : opp_of T := F.neg.
+Instance : zero_of T := F.zero.
+Instance : one_of T := Float 1%bigZ 0%bigZ.
 
+Time Eval vm_compute in blackhole _ (cholesky_seqmx (n := seq.size matrix) matrix).
+(* Finished transaction in 8.933 secs (8.92u,0.008s) (successful) with no doubling *)
+(* Finished transaction in 15.454 secs (15.444u,0.008s) (successful) *)
+(* Finished transaction in 18.175 secs (18.172u,0.004s) (successful) *)
+End test_CoqInterval_add.
+
+Require Import Float.
+Section test_prim_add.
+Local Notation T := float (only parsing).
+Instance : add_of T := fun a b => (* fun a b =>
+                         let r1 := add a b in
+                         let r2 := add b a in
+                       ((r1 + r2) / two)%float *)
+                         let r := add a b in r.
+Instance : mul_of T := mul.
+Instance : sqrt_of T := sqrt.
+Instance : div_of T := div.
+Instance : opp_of T := opp.
+Instance : zero_of T := zero.
+Instance : one_of T := one.
+Definition matrix_float := Eval compute in map (map BigZFloat2Prim) matrix.
+Time Eval vm_compute in blackhole _ (cholesky_seqmx (n := seq.size matrix_float) matrix_float).
+(* Finished transaction in 0.524 secs (0.524u,0.s) (successful) *)
+(* Finished transaction in 0.556 secs (0.555u,0.s) (successful) *)
+End test_prim_add.
+
+Set Printing All.
+Print t.
+About sqrt_of_instance_0.
 Goal True. idtac "test_CoqInterval_add". done. Qed.
-Time Eval vm_compute in let res := cholesky4 (n := seq.size m12) m12 in tt.
 
 End test_CoqInterval_add.
 
