1be6ec99c294dcb869bd30320297cc568ed55b7c
Pierre Roux <pierre.roux@onera.fr>
2018-09-14 15:33:01 +0200

    WIP

--- theories/Flocq_complements.v
+++ theories/Flocq_complements.v
@@ -4,6 +4,8 @@ Require Import Flocq.Core.Digits.
 Require Import Flocq.Core.Ulp.
 Require Import Flocq.IEEE754.Binary.
 
+Require Import Psatz.
+
 Section Bsucc_pred.
 
 Arguments exist {A} {P}.
@@ -26,15 +28,103 @@ Notation is_finite_strict := (is_finite_strict prec emax).
 Notation is_nan := (is_nan prec emax).
 Notation Bsign := (Bsign prec emax).
 Notation Bopp := (Bopp prec emax).
+Notation binary_round := (binary_round prec emax).
+Notation FF2B := (FF2B prec emax).
+Notation binary_round_correct :=
+  (binary_round_correct prec emax prec_gt_0_ Hmax).
+
+Definition Bnormfr_mantissa (x : binary_float prec emax) :=
+  match x with
+  | B754_finite _ _ _ mx ex _ =>
+    if Z.eqb ex (-prec)%Z then Npos mx else 0%N
+  | _ => 0%N
+  end.
+
+Definition Bldexp mode f e :=
+  match f with
+  | B754_finite _ _ sx mx ex _ =>
+    FF2B _ (proj1 (binary_round_correct mode sx mx (ex+e)))
+  | _ => f
+  end.
+
+(* TODO
+Theorem Bldexp_correct :
+*)
+
+Definition Ffrexp_core_binary s m e :=
+  if (digits2_pos m =? Z.to_pos prec)%positive then
+    (F754_finite s m (-prec)%Z, (e + prec)%Z)
+  else
+    let d := (prec - Z.pos (digits2_pos m))%Z in
+    (F754_finite s (shift_pos (Z.to_pos d) m) (-prec)%Z, (e + prec - d)%Z).
+
+Lemma Bfrexp_correct_aux :
+  forall sx mx ex (Hx : bounded mx ex = true),
+  let x := F2R (Float radix2 (cond_Zopp sx (Z.pos mx)) ex) in
+  let z := fst (Ffrexp_core_binary sx mx ex) in
+  let e := snd (Ffrexp_core_binary sx mx ex) in
+  valid_binary z = true /\
+  (/2 <= Rabs (FF2R radix2 z) < 1)%R /\
+  (x = FF2R radix2 z * bpow radix2 e)%R.
+Proof.
+Admitted.
+
+Definition Bfrexp f :=
+  match f with
+  | B754_finite _ _ s m e H =>
+    let e' := snd (Ffrexp_core_binary s m e) in
+    (FF2B _ (proj1 (Bfrexp_correct_aux s m e H)), e')
+  | _ => (f, (-2*emax-prec)%Z)
+  end.
+
+Theorem Bfrexp_correct :
+  forall f,
+  is_finite_strict f = true ->
+  let x := B2R f in
+  let z := fst (Bfrexp f) in
+  let e := snd (Bfrexp f) in
+  (/2 <= Rabs (B2R z) < 1)%R /\
+  (x = B2R z * bpow radix2 e)%R.
+Proof.
+intro f; case f; intro s; try discriminate; intros m e Hf _.
+generalize (Bfrexp_correct_aux s m e Hf).
+now intros (_, (Hb, Heq)); simpl; rewrite B2R_FF2B.
+Qed.
+
+(* TODO : ajouter ulp (depuis primitive_floats_infnan.v) *)
+(* TODO : redéfinir succ à l'aide de ulp (définition plus proche de succ défini dans Ulp.v de Flocq) *)
 
 Definition Fsucc_pos_core_binary m e :=
   let m' := (m + 1)%positive in
   if (digits2_pos m' <=? Z.to_pos prec)%positive then
     F754_finite false m' e
   else if (e <? emax - prec)%Z then
-    F754_finite false (shift_pos (Z.to_pos (prec - 1)) 1) (e + 1)
+    F754_finite false (shift_pos (Z.to_pos prec - 1) 1) (e + 1)
   else
     F754_infinity false.
+
+Lemma digits2_pos_succ p :
+  digits2_pos (p + 1) = digits2_pos p
+  \/ digits2_pos (p + 1) = (digits2_pos p + 1)%positive.
+Proof.
+elim p; [intros p' Hp'|intros p' Hp'|]; simpl.
+- rewrite Pos.add_1_r in Hp'.
+  destruct Hp' as [Hp'|Hp']; rewrite Hp'; [now left|right].
+  now rewrite Pos.add_succ_l.
+- now left.
+- now right.
+Qed.
+
+Lemma digits2_pos_shift_pos p p' :
+  digits2_pos (shift_pos p p') = (p + digits2_pos p')%positive.
+Proof.
+Opaque Pos.add.
+revert p'; elim p; [intros p'' Hp''|intros p'' Hp''|]; intro p'.
+- simpl; rewrite !Hp''; lia.
+- simpl; rewrite !Hp''; lia.
+- simpl; lia.
+Transparent Pos.add.
+Qed.
 
 Definition Bsucc_pos_correct_aux :
   forall mx ex (Hx : bounded mx ex = true),
@@ -47,6 +137,39 @@ Definition Bsucc_pos_correct_aux :
   else
     z = F754_infinity false.
 Proof.
+intros mx ex Bx.
+set (x := F2R _).
+set (z := Fsucc_pos_core_binary _ _).
+simpl; split.
+- unfold z, Fsucc_pos_core_binary.
+  case (Pos.leb_spec _ _); intro Hdmxp1.
+  + simpl; unfold bounded, canonical_mantissa.
+    apply andb_prop in Bx; destruct Bx as (Bmx, Bex).
+    apply andb_true_intro; split; [|exact Bex].
+    apply Zeq_bool_true.
+    unfold canonical_mantissa in Bmx.
+    apply Zeq_bool_eq in Bmx; rewrite <-Bmx at 2.
+    destruct (digits2_pos_succ mx) as [Hdmxp1'|Hdmxp1'];
+      rewrite Hdmxp1'; [now simpl|].
+    assert (H : (Z.pos (digits2_pos mx) + ex - prec < emin)%Z).
+    { destruct (Z.max_spec (Z.pos (digits2_pos mx) + ex - prec) emin)
+        as [(H1, H2)|(H1, H2)]; [exact H1|].
+      exfalso; revert Bmx; fold emin; unfold FLT_exp.
+      rewrite Hdmxp1' in Hdmxp1.
+      apply Pos2Z.pos_le_pos in Hdmxp1.
+      rewrite Pos2Z.inj_add, Z2Pos.id in Hdmxp1; [lia|exact prec_gt_0_]. }
+    fold emin; unfold FLT_exp; rewrite !Z.max_r; lia.
+  + case (Z.ltb_spec _ _); intro Hex; [|now simpl].
+    simpl; unfold bounded, canonical_mantissa.
+    apply andb_true_intro; split; [|now apply Zle_bool_true; lia].
+    apply Zeq_bool_true.
+    rewrite digits2_pos_shift_pos; unfold digits2_pos.
+    replace (Z.pos (_ + _)) with prec.
+Print succ.
+Print pred_pos.
+Search Zeq_bool.
+
+
 Admitted.
 
 Lemma smallest_positive_subnormal_proof :
@@ -156,8 +279,6 @@ Admitted.
 
 Definition Bpred pred_nan x :=
   Bopp pred_nan (Bsucc pred_nan (Bopp pred_nan x)).
-
-Require Import Psatz.
 
 Lemma Rcompare_Ropp x y : Rcompare (- x) (- y) = Rcompare y x.
 Proof.
