4b57e7d03f99e4392ad8935753c695f8a1288bf8
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2019-08-19 23:04:49 +0200

    refactor: Improve install_sdpa_on_debian_10.sh

--- install_sdpa_on_debian_10.sh
+++ install_sdpa_on_debian_10.sh
@@ -16,7 +16,10 @@ Pin: release a=testing
 Pin-Priority: 100
 EOF
 
-sudo apt-get update -y -q
+# Improved version w.r.t. "sudo apt-get update -y -q"
+exec {fd}>&2 # copy stderr to some unused fd
+travis_retry bash -o pipefail -c "sudo apt-get update -y -q 2>&1 | tee /dev/fd/$fd | ( ! grep -q -e '^Err:' -e '^E:' )"
+exec {fd}>&- # close file descriptor
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -q -t bullseye --no-install-recommends sdpa
 
