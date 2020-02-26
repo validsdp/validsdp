dfa847dc61d2cf2b548c9bc21dfd1ea0dd7c6d45
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2020-02-26 13:59:26 +0100

    Fix CI (sdpa is no longer in bullseye)

--- install_sdpa_on_debian_10.sh
+++ install_sdpa_on_debian_10.sh
@@ -2,22 +2,22 @@
 
 # This script is intended to fix the following issue:
 #
-# The sdpa package is missing in the Debian *buster* APT archive;
-# see also: https://packages.debian.org/bullseye/sdpa
+# Package sdpa is missing in the Debian APT archives buster, bullseye;
+# see also: https://packages.debian.org/sid/sdpa
 
 set -ex
 
-sudo tee /etc/apt/sources.list.d/bullseye.list \
-     <<<"deb http://deb.debian.org/debian bullseye main"
+sudo tee /etc/apt/sources.list.d/sid.list \
+     <<<"deb http://deb.debian.org/debian sid main"
 
-sudo tee /etc/apt/preferences.d/00bullseye-pin <<EOF
+sudo tee /etc/apt/preferences.d/00sid-pin <<EOF
 Package: *
-Pin: release a=testing
+Pin: release n=sid
 Pin-Priority: 100
 EOF
 
 sudo apt-get update -y -q
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -q -t bullseye --no-install-recommends sdpa
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -q -t sid --no-install-recommends sdpa
 
 : sdpa installed
