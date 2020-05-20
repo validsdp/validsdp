3b090db6e3f4607b5410cdff0b9c0271b0d9d9bb
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2020-05-20 12:02:23 +0200

    fix: install_sdpa_on_debian_10.sh

--- install_sdpa_on_debian_10.sh
+++ install_sdpa_on_debian_10.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-# This script is intended to fix the following issue:
+# This script is intended to workaround the following issue:
 #
-# Package sdpa is missing in the Debian APT archives buster, bullseye;
-# see also: https://packages.debian.org/sid/sdpa
+# Package sdpa is missing in the Debian APT archive(s) buster;
+# see: https://packages.debian.org/bullseye/sdpa
+# and: https://packages.debian.org/sid/sdpa
+#
+# Beware: this script may break your Debian distribution; use it with
+# caution! - all the more as SDPA is an optional ValidSDP dependency.
 
 set -ex
 
-sudo tee /etc/apt/sources.list.d/sid.list \
-     <<<"deb http://deb.debian.org/debian sid main"
+sudo tee /etc/apt/sources.list.d/bullseye.list \
+     <<<"deb http://deb.debian.org/debian bullseye main"
 
-sudo tee /etc/apt/preferences.d/00sid-pin <<EOF
+sudo tee /etc/apt/preferences.d/00bullseye-pin <<EOF
 Package: *
-Pin: release n=sid
+Pin: release a=testing
 Pin-Priority: 100
 EOF
 
 sudo apt-get update -y -q
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -q -t sid --no-install-recommends sdpa
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -q -t bullseye --no-install-recommends sdpa
 
 : sdpa installed
